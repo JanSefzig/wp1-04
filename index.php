4caba6471ee32c745979f2b4d179f918884d9c33
JanSefzig <nicolas.jan.sefzig@student.ossp.cz>
2021-03-23 11:58:46 +0100

    ok

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+$temperature = 56?
+
+if($temperature >= 100) [
+    echo "Můžu jít na kolotoč";
+] elseif(temperature < 50 ) [
+    echo "Nemůžu jít nikam";
+] else {
+    echo "Počet peněz je: $temperature = Můžu jít na autodrom"
+]
+
+if($password = "heslo123") [
+    echo "užij si to";
+]
+
+
+    
+</body>
+</html>
